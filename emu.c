16bd26b95086917ebb385f4bc176ffdc9d4c6ad7
Colin Maykish <crmaykish@gmail.com>
2015-12-07 23:29:01 -0500

    A instruction implemented, processor fetch/decode framework set up

--- emu.c
+++ emu.c
@@ -12,19 +12,31 @@ int IM[32768];
 // Data Memory (RAM)
 int DM[32768];
 
-int comp(int opcode);
-int dest(int opcode);
-int jump(int opcode);
+// Program Counter
+int PC = 0;
+
+// Latest Op Code
+int OP = 0;
+
+int a_val(int opcode);
+int c_comp(int opcode);
+int c_dest(int opcode);
+int c_jump(int opcode);
+int cycle();
+void debug();
 
 int main(){
 	int running = 1;
-	A = 0;
-	D = 0;
+
+	IM[0] = 5;
 
 	printf("%s\n", "Starting emulation...");
 	
 	while (running){
-		running = instruction();
+		running = cycle();
+		debug();
+		PC++;
+		getchar();
 	}
 
 	printf("%s\n", "Emulation stopped.");
@@ -32,19 +44,44 @@ int main(){
 	return 0;
 }
 
-int instruction(){
+int cycle(){
+	int status_code = 0;	// 0 = instruction failed, 1 = instruction succeeded
 
-	return 0;	// bad instruction
+	// Fetch Op Code
+	OP = IM[PC];
+
+	// Execute Op Code
+	if (OP & 0b1000000000000000){
+		// C Instruction
+		status_code = 0;
+	}
+	else {
+		// A Instruction
+		A = a_val(OP);
+		status_code = 1;
+	}
+
+	return status_code;
 }
 
-int comp(int opcode){
+int a_val(int opcode){
+	// The AND is probably not necessary if we've already determined this an A instruction
+	return (opcode & 0b111111111111111);
+}
+
+int c_comp(int opcode){
 	return (opcode & 0b1111111000000) >> 6;
 }
 
-int dest(int opcode){
+int c_dest(int opcode){
 	return (opcode & 0b111000) >> 3;
 }
 
-int jump(int opcode){
+int c_jump(int opcode){
 	return opcode & 0b111;
+}
+
+void debug(){
+	printf("%d => %X | A: %X, D: %X\n", PC, OP, A, D);
+	printf("%s\n", "---");
 }
