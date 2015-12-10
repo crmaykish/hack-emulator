5fd32497c8229b57134cb4930fa691bdfd977ba1
Dingus McGee <dingus@debian>
2015-12-10 13:38:44 -0500

    Assembler loads ASM files into memory

---
+++ assembler/asm_files/loop.asm
@@ -0,0 +1,8 @@
+// Repeatedly increment i
+		@i
+		M=1
+(LOOP)
+		@i
+		M=M+1
+		@LOOP
+		0;JMP
--- assembler/assembler.c
+++ assembler/assembler.c
@@ -1,11 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "parser.h"
 
-int main(int argc, char* argv[]){
-	Parser p;
-	char *file_contents = NULL;
+char *file_contents;
 
+int load_asm(char *asm_path);
+
+int main(int argc, char *argv[]){
+	// Primitive argument checking
+	if (argc < 3) {
+		printf("%s\n", "Invalid arguments.");
+		return 1;
+	}
+	
+	if (load_asm(argv[1])){
+		printf("%s\n", "Problem loading ASM file.");
+		return 1;
+	}
+	
+	Parser p;
 	parser_init(&p, file_contents);
 
 	return 0;
+}
+
+int load_asm(char *asm_path){
+	int status_code = 0;
+	FILE *asm_file = fopen(asm_path, "r");
+
+	if (asm_file == NULL){
+		status_code = 1;
+	}
+	else {
+		long file_length = 0;
+		char c = 0;
+
+		fseek(asm_file, 0, SEEK_END);
+		file_length = ftell(asm_file);
+		fseek(asm_file, 0, SEEK_SET);
+
+		file_contents = malloc(file_length);
+
+		if (file_contents){
+			fread(file_contents, 1, file_length, asm_file);
+		}
+
+		fclose(asm_file);
+	}
+
+	return status_code;
 }
