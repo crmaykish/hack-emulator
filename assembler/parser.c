4312f5e7b03e27fbca38359e74b0f147f6566f7e
Dingus McGee <dingus@debian>
2015-12-10 09:42:39 -0500

    Assembler makefile

--- assembler/parser.c
+++ assembler/parser.c
@@ -24,20 +24,16 @@ Command_Type command_type(Parser *parser) {
 
 char *symbol(Parser *parser) {
 
-	return NULL;
 }
 
 char *dest(Parser *parser) {
 
-	return NULL;
 }
 
 char *comp(Parser *parser) {
 
-	return NULL;
 }
 
 char *jump(Parser *parser) {
 
-	return NULL;
 }
