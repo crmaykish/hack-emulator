386d7e423601c775279da8a074aded4d46046bd2
crmaykish <crmaykish@gmail.com>
2018-08-04 20:09:12 -0400

    Stub VM code generator implentation

--- translator/include/vm_code_generator.hpp
+++ translator/include/vm_code_generator.hpp
@@ -1,8 +1,16 @@
 #ifndef VM_CODE_GENERATOR_HPP
 #define VM_CODE_GENERATOR_HPP
 
+#include <string>
+
 class VMCodeGenerator {
 public:
+    VMCodeGenerator();
+    
+    std::string Generate(std::string vmCode);
+
+    std::string Arithmetic(std::string command);
+    std::string PushPop(std::string command);
 
 private:
 
